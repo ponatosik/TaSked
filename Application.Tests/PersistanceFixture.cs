407f5d57aa85d84bec3470ae1df864a8f208a18d
ponatosik <ponatosik@gmail.com>
2024-03-06 16:33:33 +0200

    test: Set up test project

---
+++ Application.Tests/PersistanceFixture.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TaSked.Infrastructure.Persistance;
+
+namespace Application.Tests;
+
+// A class to share context between tests.
+// About Xunit Fixtures: https://xunit.net/docs/shared-context#collection-fixture
+public class PersistanceFixture : IDisposable
+{
+	private ApplicationDbContext _dbContext;
+	public ApplicationDbContext GetDbContext()
+	{
+		_dbContext.Database.EnsureDeleted();
+		return _dbContext;
+	}
+
+	public PersistanceFixture()
+	{
+		var options = new DbContextOptionsBuilder<ApplicationDbContext>()
+			.UseInMemoryDatabase("TestDB")
+			.Options;
+
+		_dbContext = new ApplicationDbContext(options);
+	}
+
+	public void Dispose()
+	{
+		_dbContext.Dispose();
+	}
+}
+
+[CollectionDefinition("Persistance tests")]
+public class PersistanceTestsCollection : ICollectionFixture<PersistanceFixture>;
