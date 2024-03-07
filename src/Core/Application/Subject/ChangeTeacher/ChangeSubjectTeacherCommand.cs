9e66ef4ae78555a36f8e182838037bda0e0e9352
Ridcovets_Sergo <springtrapukrainian@gmail.com>
2024-03-07 11:48:20 +0200

    Created "Change Subject Teacher"

---
+++ src/Core/Application/Subject/ChangeTeacher/ChangeSubjectTeacherCommand.cs
@@ -0,0 +1,6 @@
+﻿using TaSked.Domain;
+using MediatR;
+
+namespace TaSked.Application;
+
+public record ChangeSubjectTeacherCommand(Guid UserId, Guid SubjectId, Teacher NewSubjectTeacher) : IRequest<Subject>;
