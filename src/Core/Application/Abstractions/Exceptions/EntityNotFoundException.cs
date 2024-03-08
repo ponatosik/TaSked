381e63ec44f94a1a3796247b97e492fc9438bc7f
ponatosik <ponatosik@gmail.com>
2024-03-08 13:03:47 +0200

    feat: Add applications exceptons

---
+++ src/Core/Application/Abstractions/Exceptions/EntityNotFoundException.cs
@@ -0,0 +1,23 @@
+﻿namespace TaSked.Application.Exceptions;
+
+public class EntityNotFoundException : ApplicationException
+{
+	public Guid EntityId { get; private set; }
+	public string EntityName { get; private set; }
+	
+	private static string GenerateMessage(Guid id, string name) => 
+		$"{name} with id \"{id}\" was not found.";
+
+	internal EntityNotFoundException(Guid entityId, string entityName) 
+		: base(GenerateMessage(entityId, entityName))
+	{
+		EntityId = entityId;
+		EntityName = entityName;
+	}
+	internal EntityNotFoundException(Guid entityId, string entityName, Exception inner)
+		: base(GenerateMessage(entityId, entityName), inner)
+	{
+		EntityId = entityId;
+		EntityName = entityName;
+	}
+}
