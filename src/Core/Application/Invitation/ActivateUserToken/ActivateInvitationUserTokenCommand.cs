f17313fd2ae85f561cdb46da069f9d85d8314419
Ridcovets_Sergo <springtrapukrainian@gmail.com>
2024-03-08 18:25:57 +0200

    Created "Activate User Token" for Invintation

---
+++ src/Core/Application/Invitation/ActivateUserToken/ActivateInvitationUserTokenCommand.cs
@@ -0,0 +1,6 @@
+﻿using TaSked.Domain;
+using MediatR;
+
+namespace TaSked.Application;
+
+public record ActivateInvitationUserTokenCommand(Guid UserId, Guid InvitationId, Guid GroupId) : IRequest<string>;
---
+++ src/Core/Application/Invitation/ActivateUserToken/ActivateInvitationUserTokenCommandHandler.cs
@@ -0,0 +1,31 @@
+﻿using TaSked.Application.Data;
+using TaSked.Domain;
+using MediatR;
+
+namespace TaSked.Application;
+
+public class ActivateInvitationUserTokenCommandHandler : IRequestHandler<ActivateInvitationUserTokenCommand, string>
+{
+    private readonly IApplicationDbContext _context;
+    private readonly IJwtProvider _jwtProvider;
+
+    public ActivateInvitationUserTokenCommandHandler(IApplicationDbContext context,  IJwtProvider jwtProvider)
+    {
+        _context = context;
+        _jwtProvider = jwtProvider;
+    }
+
+    public async Task<string> Handle(ActivateInvitationUserTokenCommand request, CancellationToken cancellationToken)
+    {
+        var user = _context.Users.FindById(request.UserId);
+        var group = _context.Groups.FindById(request.GroupId);
+        var invitation = group.Invitations.FindById(request.InvitationId);
+
+        group.JoinByInvintation(invitation, user);
+
+        await _context.SaveChangesAsync(cancellationToken);
+
+        string token = _jwtProvider.Generate(user);
+        return token;
+    }
+}
