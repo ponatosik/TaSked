f1c9f26096d7a5fc07bba8da1afa83af41ffc128
ponatosik <ponatosik@gmail.com>
2024-03-08 11:50:05 +0200

    Add policy-based authorization

---
+++ src/Infrastructure/Authorization/MinimalGroupRoleRequirmentHandler.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using TaSked.Application.Data;
+using TaSked.Domain;
+using TaSked.Infrastructure.Authorization;
+
+namespace Infrastructure.Authorization;
+
+public class MinimalGroupRoleRequirmentHandler : AuthorizationHandler<MinimalGroupRoleRequirment>
+{
+	private readonly IApplicationDbContext _dbContext;
+	public MinimalGroupRoleRequirmentHandler(IApplicationDbContext dbContext) 
+	{
+		_dbContext = dbContext;
+	}
+
+	protected override Task HandleRequirementAsync(AuthorizationHandlerContext context, MinimalGroupRoleRequirment requirement)
+	{
+		GroupRole requiredRole = requirement.GroupRole;
+		Guid? userId = context.User.GetUserId();
+		if (userId == null)
+		{
+			context.Fail();
+			return Task.CompletedTask;
+		}
+
+		User? user = _dbContext.Users.FirstOrDefault(user => user.Id == userId);
+		if (user == null)
+		{
+			context.Fail();
+			return Task.CompletedTask;
+		}
+		if (user.Role < requiredRole)
+		{
+			context.Fail();
+			return Task.CompletedTask;
+		}
+
+		context.Succeed(requirement);
+		return Task.CompletedTask;
+	}
+}
---
+++ src/Infrastructure/Authorization/Policies.cs
@@ -0,0 +1,8 @@
+﻿namespace TaSked.Infrastructure.Authorization;
+
+public static class AccessPolicise
+{
+	public const string Member = "member";
+	public const string Moderator = "moderator";
+	public const string Admin = "admin";
+}
