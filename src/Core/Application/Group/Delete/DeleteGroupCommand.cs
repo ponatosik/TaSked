efac1572ecc8266177dc11b14bd99ec53b31cf85
Ridcovets_Sergo <springtrapukrainian@gmail.com>
2024-03-08 11:10:34 +0200

    Created "Delete" for Group

---
+++ src/Core/Application/Group/Delete/DeleteGroupCommand.cs
@@ -0,0 +1,6 @@
+﻿using TaSked.Domain;
+using MediatR;
+
+namespace TaSked.Application;
+
+public record DeleteGroupCommand(Guid UserId) : IRequest;
---
+++ src/Core/Application/Group/Delete/DeleteGroupCommandHandler.cs
@@ -0,0 +1,41 @@
+﻿using TaSked.Application.Data;
+using TaSked.Domain;
+using MediatR;
+
+namespace TaSked.Application;
+
+public class DeleteGroupCommandHandler : IRequestHandler<DeleteGroupCommand>
+{
+    private readonly IApplicationDbContext _context;
+
+    public DeleteGroupCommandHandler(IApplicationDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task Handle(DeleteGroupCommand request, CancellationToken cancellationToken)
+    {
+        var user = _context.Users.FirstOrDefault(user => user.Id == request.UserId);
+        if (user == null)
+        {
+            throw new Exception("No such user found");
+        }
+        if (user.GroupId == null)
+        {
+            throw new Exception("User is not a member of a group");
+        }
+        if (user.Role == GroupRole.Member)
+        {
+            throw new Exception("No permision to create subjects");
+        }
+
+        var group = _context.Groups.FirstOrDefault(group => group.Id == user.GroupId);
+        if (group != null)
+        {
+            _context.Groups.Remove(group);
+        }
+
+        await _context.SaveChangesAsync(cancellationToken);
+
+    }
+}
