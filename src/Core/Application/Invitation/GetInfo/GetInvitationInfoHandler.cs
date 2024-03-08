362b5fd274e83e1ebbc4610b41ebf46fd6d4575b
Ridcovets_Sergo <springtrapukrainian@gmail.com>
2024-03-08 12:58:03 +0200

    Created "Get info" for Invitation

---
+++ src/Core/Application/Invitation/GetInfo/GetInvitationInfoHandler.cs
@@ -0,0 +1,37 @@
+﻿using MediatR;
+using Microsoft.EntityFrameworkCore;
+using TaSked.Application.Data;
+using TaSked.Domain;
+
+namespace TaSked.Application;
+
+public class GetInvitationInfoHandler : IRequestHandler<GetInvitationInfoQuery, List<Invitation>>
+{
+    private readonly IApplicationDbContext _context;
+
+    public GetInvitationInfoHandler(IApplicationDbContext context)
+    {
+        _context = context;
+    }
+
+    public Task<List<Invitation>> Handle(GetInvitationInfoQuery request, CancellationToken cancellationToken)
+    {
+        var user = _context.Users.FirstOrDefault(user => user.Id == request.UserId);
+        if (user == null)
+        {
+            throw new Exception("No such user found");
+        }
+        if (user.GroupId == null)
+        {
+            throw new Exception("User is not a member of a group");
+        }
+
+        var group = _context.Groups.FirstOrDefault(group => group.Id == user.GroupId);
+        if (group == null)
+        {
+            throw new Exception("Unknown Group");
+        }
+
+        return Task.FromResult(group.Invitations);
+    }
+}
---
+++ src/Core/Application/Invitation/GetInfo/GetInvitationInfoQuery.cs
@@ -0,0 +1,6 @@
+﻿using MediatR;
+using TaSked.Domain;
+
+namespace TaSked.Application;
+
+public record GetInvitationInfoQuery(Guid UserId) : IRequest<List<Invitation>>;
