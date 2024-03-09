0a2390c00cb27c259e737f9d0b0100a384a0e987
Ridcovets_Sergo <springtrapukrainian@gmail.com>
2024-03-09 13:28:28 +0200

    Created Controller for Group

--- src/Presentation/TaSked/Controllers/GroupController.cs
+++ src/Presentation/TaSked/Controllers/GroupController.cs
@@ -10,6 +10,7 @@ namespace TaSked.Api.Controllers;
 
 [ApiController]
 [Route("[controller]")]
+[Authorize]
 public class GroupController : ControllerBase
 {
 	private readonly IMediator _mediator;
@@ -20,10 +21,40 @@ public class GroupController : ControllerBase
 	}
 	
 	[HttpPost]
-	[Authorize]
 	public async Task<IActionResult> Post(CreateGroupRequest request)
 	{
 		Guid userId = User.GetUserId()!.Value;
 		return Ok(await _mediator.Send(new CreateGroupCommand(userId, request.GroupName)));
 	}
+
+    [HttpDelete]
+    public async Task<IActionResult> Delete()
+    {
+        Guid userId = User.GetUserId()!.Value;
+		await _mediator.Send(new DeleteGroupCommand(userId));
+        return Ok();
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Post(ChangeGroupNameRequest request)
+    {
+        Guid userId = User.GetUserId()!.Value;
+        return Ok(await _mediator.Send(new ChangeGroupNameCommand(userId, request.GroupName)));
+    }
+
+    [HttpGet]
+    [Route("Members")]
+    public async Task<IActionResult> Get()
+    {
+        Guid userId = User.GetUserId()!.Value;
+        return Ok(await _mediator.Send(new GetGroupMembersQuery(userId)));
+    }
+
+    [HttpPatch]
+    public async Task<IActionResult> Patch()
+    {
+        Guid userId = User.GetUserId()!.Value;
+        await _mediator.Send(new LeaveGroupCommand(userId));
+        return Ok();
+    }
 }
