21f5338bdc00cc350174f97773377dc91f03c7e8
ponatosik <ponatosik@gmail.com>
2024-03-09 14:06:28 +0200

    add dynamic configuration

--- src/Presentation/TaSked/Program.cs
+++ src/Presentation/TaSked/Program.cs
@@ -1,22 +1,27 @@
 using TaSked.Application;
 using TaSked.Infrastructure.Persistance;
+using TaSked.Infrastructure.Persistance.AzureMySqlInApp;
 using TaSked.Infrastructure.Authorization;
 using TaSked.Infrastructure.ExceptionHandling;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+
+bool useAzureMySqlInApp = Environment.GetEnvironmentVariable("UseMySqlInApp") == "true";
+string JwtSecretKey = builder.WebHost.GetSetting("JwtSecretKey") ?? "{774F9515-F749-42F1-8578-8BA810C3BA78}";
+string baseUrls = builder.WebHost.GetSetting(WebHostDefaults.ServerUrlsKey) ?? "http://localhost:5070";
+var baseUrlList = baseUrls.Split(';');
 
 builder.Services.AddControllers();
-builder.Services.AddPersistance();
 builder.Services.AddMediatR(config => config.RegisterServicesFromAssembly(typeof(CreateHomeworkCommand).Assembly));
+builder.Services.AddPolicyBasedAuthorization();
+builder.Services.AddPersistance(useAzureMySqlInApp ? opt => opt.UseAzureMysqlInApp() : null);
 builder.Services.AddJwtAuthentication(options =>
 {
-	options.Issuer = "https://localhost:5070/";
-	options.Audience = "https://localhost:5070/";
-	options.SecretKey = "{774F9515-F749-42F1-8578-8BA810C3BA78}";
+	options.Issuer = baseUrlList.First();
+	options.Audience = baseUrlList.First();
+	options.SecretKey = JwtSecretKey;
 });
-builder.Services.AddPolicyBasedAuthorization();
 
 var app = builder.Build();
 
