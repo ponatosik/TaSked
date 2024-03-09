5afd7353ed10bc1090392cf500c5f6fc46803761
Ridcovets_Sergo <springtrapukrainian@gmail.com>
2024-03-09 15:00:19 +0200

    Created Controller for Lesson

---
+++ src/Presentation/TaSked/Controllers/LessonController.cs
@@ -0,0 +1,58 @@
+﻿using TaSked.Domain;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using TaSked.Application;
+using Microsoft.AspNetCore.Authorization;
+using TaSked.Infrastructure.Authorization;
+using TaSked.Api.Requests;
+
+namespace TaSked.Api.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+[Authorize]
+public class LessonController : ControllerBase
+{
+    private readonly IMediator _mediator;
+
+    public LessonController(IMediator mediator)
+    {
+        _mediator = mediator;
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Post(CreateLessonRequest request)
+    {
+        Guid userId = User.GetUserId()!.Value;
+        return Ok(await _mediator.Send(new CreateLessonCommand(userId, request.SubjectId, request.LessonTime)));
+    }
+
+    [HttpDelete]
+    public async Task<IActionResult> Delete(DeleteLessonRequest request)
+    {
+        Guid userId = User.GetUserId()!.Value;
+        await _mediator.Send(new DeleteLessonCommand(userId, request.SubjectId, request.LessonId));
+        return Ok();
+    }
+
+    [HttpPatch]
+    public async Task<IActionResult> Patch(ChangeLessonTimeRequest request)
+    {
+        Guid userId = User.GetUserId()!.Value;
+        return Ok(await _mediator.Send(new ChangeLessonTimeCommand(userId, request.SubjectId, request.LessonId, request.NewTime)));
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> Get(GetAllLessonsBySubjectRequest request)
+    {
+        Guid userId = User.GetUserId()!.Value;
+        return Ok(await _mediator.Send(new GetAllLessonsBySubjectQuery(userId, request.SubjectId)));
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> Get(GetAllLessonsInDateRangeRequest request)
+    {
+        Guid userId = User.GetUserId()!.Value;
+        return Ok(await _mediator.Send(new GetAllLessonsInDateRangeQuery(userId, request.StartDate, request.EndDate)));
+    }
+}
