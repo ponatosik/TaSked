a777dd78049cc8b1ac5343e347bbb7e133883d0f
Ridcovets_Sergo <springtrapukrainian@gmail.com>
2024-03-09 15:32:10 +0200

    Created Controller for Report

---
+++ src/Presentation/TaSked/Controllers/ReportController.cs
@@ -0,0 +1,37 @@
+﻿using TaSked.Domain;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using TaSked.Application;
+using Microsoft.AspNetCore.Authorization;
+using TaSked.Infrastructure.Authorization;
+using TaSked.Api.Requests;
+
+namespace TaSked.Api.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+[Authorize(AccessPolicise.Member)]
+public class ReportController : ControllerBase
+{
+    private readonly IMediator _mediator;
+
+    public ReportController(IMediator mediator)
+    {
+        _mediator = mediator;
+    }
+
+    [HttpPost]
+    [Authorize(AccessPolicise.Moderator)]
+    public async Task<IActionResult> Post(CreateReportRequest request)
+    {
+        Guid userId = User.GetUserId()!.Value;
+        return Ok(await _mediator.Send(new CreateReportCommand(userId, request.ReportTitle, request.ReportMessage)));
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> Get()
+    {
+        Guid userId = User.GetUserId()!.Value;
+        return Ok(await _mediator.Send(new GetAllReportQuery(userId)));
+    }
+}
