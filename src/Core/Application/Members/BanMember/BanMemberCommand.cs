11f422d1eb8fb97c93dd13cbad5a6d5ce30b57d6
ponatosik <ponatosik@gmail.com>
2024-03-11 15:52:25 +0200

    feat: Ban members

---
+++ src/Core/Application/Members/BanMember/BanMemberCommand.cs
@@ -0,0 +1,6 @@
+﻿using MediatR;
+using TaSked.Domain;
+
+namespace TaSked.Application;
+
+public record BanMemberCommand(Guid BannedBy, Guid GroupId, Guid UserId) : IRequest;
--- src/Presentation/TaSked/Requests/MemberRequests.cs
+++ src/Presentation/TaSked/Requests/MemberRequests.cs
@@ -2,3 +2,4 @@
 
 public record PromoteMemberRequest (Guid UserId);
 public record DemoteMemberRequest (Guid UserId);
+public record BanMemberRequest (Guid UserId);
