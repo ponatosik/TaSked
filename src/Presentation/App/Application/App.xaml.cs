7298f144ea587857d09162d882ed293e138b9a14
Ridcovets_Sergo <springtrapukrainian@gmail.com>
2024-03-14 10:39:31 +0200

    Dark theme selected

--- src/Presentation/App/Application/App.xaml.cs
+++ src/Presentation/App/Application/App.xaml.cs
@@ -5,7 +5,7 @@ public partial class App : Microsoft.Maui.Controls.Application
 	public App()
 	{
 		InitializeComponent();
-
+        Microsoft.Maui.Controls.Application.Current.UserAppTheme = AppTheme.Dark;
 		MainPage = new AppShell();
 	}
 }
