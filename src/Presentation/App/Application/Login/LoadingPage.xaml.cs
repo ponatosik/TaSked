b5167626e7465c156959e5c9b45203bccbae3af6
Ridcovets_Sergo <springtrapukrainian@gmail.com>
2024-03-16 01:01:04 +0200

    Loading Page Update and commented

--- src/Presentation/App/Application/Login/LoadingPage.xaml.cs
+++ src/Presentation/App/Application/Login/LoadingPage.xaml.cs
@@ -1,9 +1,26 @@
+using TaSked.App.Common;
+
 namespace TaSked.App;
 
 public partial class LoadingPage : ContentPage
 {
-	public LoadingPage()
+    private readonly LoginService _loginService;
+    public LoadingPage(LoginService loginService)
 	{
 		InitializeComponent();
-	}
+        _loginService = loginService;
+    }
+
+    protected override async void OnNavigatedTo(NavigatedToEventArgs args)
+    {
+        if (await _loginService.IsAuthorized())
+        {
+            await Shell.Current.GoToAsync("//MainPage");
+        }
+        else
+        {
+            await Shell.Current.GoToAsync("//LoginPage");
+        }
+        base.OnNavigatedTo(args);
+    }
 }
