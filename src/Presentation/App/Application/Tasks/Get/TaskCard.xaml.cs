d6c30d18efc94d8fd8b908e429ea9e4294289c53
ponatosik <ponatosik@gmail.com>
2024-03-21 10:47:36 +0200

    feat: add conditional rendering

--- src/Presentation/App/Application/Tasks/Get/TaskCard.xaml.cs
+++ src/Presentation/App/Application/Tasks/Get/TaskCard.xaml.cs
@@ -5,7 +5,7 @@ namespace TaSked.App.Components;
 public partial class TaskCard : ContentView
 {
 	public static readonly BindableProperty ModelProperty =
-		BindableProperty.Create(nameof(Model), typeof(TaskViewModel), typeof(TaskCard));
+		BindableProperty.Create(nameof(Model), typeof(TaskViewModel), typeof(TaskCard), propertyChanged: OnModelChanged);
 
 	public TaskViewModel Model
 	{
@@ -16,5 +16,15 @@ public partial class TaskCard : ContentView
 	public TaskCard()
 	{
 		InitializeComponent();
+	}
+
+	static void OnModelChanged(BindableObject bindable, object oldValue, object newValue)
+	{
+		TaskCard? card = bindable as TaskCard;
+		TaskViewModel? taskView = newValue as TaskViewModel;
+		if (card is not null && taskView is not null) 
+		{
+			card.Opacity = taskView.Task.Completed ? 0.6 : 1;
+		}
 	}
 }
