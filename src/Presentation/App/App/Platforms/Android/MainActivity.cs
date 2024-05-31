2c429e3af6d78918687b117db45dbff98450b947
Ridcovets_Sergo <springtrapukrainian@gmail.com>
2024-05-31 18:11:39 +0300

    NavigationBar and StatusBar color was changed

--- src/Presentation/App/App/Platforms/Android/MainActivity.cs
+++ src/Presentation/App/App/Platforms/Android/MainActivity.cs
@@ -7,5 +7,11 @@ namespace TaSked.App
 	[Activity(Theme = "@style/Maui.SplashTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
 	public class MainActivity : MauiAppCompatActivity
 	{
-	}
+        protected override void OnCreate(Bundle? savedInstanceState)
+        {
+            Window?.SetStatusBarColor(Android.Graphics.Color.Black);
+            Window?.SetNavigationBarColor(Android.Graphics.Color.Black);
+            base.OnCreate(savedInstanceState);
+        }
+    }
 }
