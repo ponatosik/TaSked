1d9e7c9a3a40ec44a5f325747db0a19bbf789573
ponatosik <ponatosik@gmail.com>
2025-02-17 20:10:23 +0200

    feat(api): update homework validation rules
     - Make Related links optional
     - Make Description optional

--- src/Presentation/Api/Api/Validators/Requests/Homework/CreateHomeworkRequestValidator.cs
+++ src/Presentation/Api/Api/Validators/Requests/Homework/CreateHomeworkRequestValidator.cs
@@ -8,10 +8,13 @@ public class CreateHomeworkRequestValidator : AbstractValidator<CreateHomeworkRe
 	public CreateHomeworkRequestValidator()
 	{
 		RuleFor(x => x.Title).NotEmpty().Length(2, 50);
-		RuleFor(x => x.Description).NotEmpty().MaximumLength(4096);
-		RuleForEach(x => x.RelatedLinks).SetValidator(new RelatedLinkValidator());
+		RuleFor(x => x.Description).NotNull().MaximumLength(4096);
+		RuleForEach(x => x.RelatedLinks)
+			.SetValidator(new RelatedLinkValidator())
+			.When(x => x.RelatedLinks is not null);
 		RuleFor(x => x.RelatedLinks)
-			.Must(x => x.Count <= 10)
+			.Must(x => x!.Count <= 10)
+			.When(x => x.RelatedLinks is not null)
 			.WithMessage("Number of {PropertyName} cannot be greater than 10");
 		RuleFor(x => x.BriefSummary)
 			.MaximumLength(150)
