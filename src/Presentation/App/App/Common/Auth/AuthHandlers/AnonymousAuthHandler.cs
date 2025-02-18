e867e9ccd9e5df77a26396f5e1c17162297242d0
ponatosik <ponatosik@gmail.com>
2025-02-18 14:21:49 +0200

    feat(app): localize error messages for auth errors

--- src/Presentation/App/App/Common/Auth/AuthHandlers/AnonymousAuthHandler.cs
+++ src/Presentation/App/App/Common/Auth/AuthHandlers/AnonymousAuthHandler.cs
@@ -1,4 +1,7 @@
-﻿using TaSked.Api.ApiClient;
+﻿using LocalizationResourceManager.Maui;
+using Refit;
+using System.Net;
+using TaSked.Api.ApiClient;
 using TaSked.Api.Requests;
 
 namespace TaSked.App.Common;
@@ -9,18 +12,30 @@ public class AnonymousAuthorizationHandler : IAuthHandler<AnonymousLoginRequest>
 {
 	private readonly IUserTokenStore _tokenStore;
 	private readonly ITaSkedService _api;
+	private readonly ILocalizationResourceManager _localizationManager;
 
-	public AnonymousAuthorizationHandler(IUserTokenStore tokenStore, ITaSkedService api)
+	public AnonymousAuthorizationHandler(IUserTokenStore tokenStore, ITaSkedService api,
+		ILocalizationResourceManager localizationManager)
 	{
 		_tokenStore = tokenStore;
 		_api = api;
+		_localizationManager = localizationManager;
 	}
 
 	public async Task LoginAsync(AnonymousLoginRequest request)
 	{
 		var userName = request.Username;
-		var token = await _api.RegisterAnonymous(new CreateAnonymousUserTokenRequest(userName));
-		_tokenStore.AccessToken = token;
+
+		try
+		{
+			var token = await _api.RegisterAnonymous(new CreateAnonymousUserTokenRequest(userName));
+			_tokenStore.AccessToken = token;
+		}
+		catch (ApiException exception) when (exception.StatusCode is HttpStatusCode.Conflict)
+		{
+			var errorMessage = string.Format(_localizationManager["Login_Authorization_Error_NicknameTaken"], userName);
+			throw new AuthenticationException(errorMessage);
+		}
 	}
 
 	public Task RestoreSessionAsync()
