a86ec431644773c142d324519bb985ef198cca56
Ridcovets_Sergo <springtrapukrainian@gmail.com>
2025-02-18 15:21:03 +0200

    feat(app): add check/uncheck task button

--- src/Presentation/App/App/Application/Tasks/Details/TasksDetailsViewModel.cs
+++ src/Presentation/App/App/Application/Tasks/Details/TasksDetailsViewModel.cs
@@ -10,37 +10,25 @@ using TaSked.Domain;
 
 namespace TaSked.App;
 
-[QueryProperty(nameof(Homework), "homework")]
+[QueryProperty(nameof(TaskViewModel), "task")]
 public partial class TasksDetailsViewModel : ObservableObject
 {
 	private readonly ILocalizationResourceManager _localizationResourceManager;
 	
 	private ITaSkedHomeworks _homeworkService;
-
-	[ObservableProperty]
-	private Homework _homework;
 	
 	[ObservableProperty]
-    private IReactiveCommand _updateHomeworkCommand;
+	private TaskViewModel _taskViewModel;
     
 	[ObservableProperty]
     private IReactiveCommand _deleteHomeworkCommand;
     
     public TasksDetailsViewModel(ITaSkedHomeworks homeworkService, ILocalizationResourceManager localizationResourceManager)
     {
-	    UpdateHomeworkCommand = ReactiveCommand.CreateFromTask(UpdateHomework);
 	    DeleteHomeworkCommand = ReactiveCommand.CreateFromTask(DeleteHomework);
 
 	    _homeworkService = homeworkService;
 	    _localizationResourceManager = localizationResourceManager;
-    }
-    
-    private async Task UpdateHomework()
-    {
-        await Shell.Current.GoToAsync("UpdateTaskPage", new Dictionary<string, object>
-        {
-            ["homework"] = Homework
-        });
     }
 
     private async Task DeleteHomework()
@@ -49,17 +37,12 @@ public partial class TasksDetailsViewModel : ObservableObject
         await popup.IndicateTaskRunningAsync(async () =>
         {
             ITaSkedHomeworks api = ServiceHelper.GetService<ITaSkedHomeworks>();
-            await api.DeleteHomework(Homework.SubjectId, Homework.Id);
+            await api.DeleteHomework(TaskViewModel.Task.Homework.SubjectId, TaskViewModel.Task.Homework.Id);
 
             HomeworkDataSource homeworkSource = ServiceHelper.GetService<HomeworkDataSource>();
-            homeworkSource.HomeworkSource.Remove(Homework.Id);
+            homeworkSource.HomeworkSource.Remove(TaskViewModel.Task.Homework.Id);
         });
         
         await Shell.Current.GoToAsync("..");
     }
-
-    public string LessonUrlDisplay =>
-	    Homework?.RelatedLinks.Count != 0
-		    ? string.Join(" ", Homework!.RelatedLinks.Select(link => $"{link.Title}: {link:Url}"))
-		    : _localizationResourceManager["Details_None"];
 }
